72cd4f5182ca11e25ce709278bbf1c769ed79b22
Gianluca De Cicco <gnlcdccc@gmail.com>
2018-05-08 12:55:39 +0200

    remove chmod

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ BIND_DATA_DIR=${DATA_DIR}/bind
 WEBMIN_DATA_DIR=${DATA_DIR}/webmin
 
 create_bind_data_dir() {
-  mkdir -p ${BIND_DATA_DIR}
+  mkdir -m 0775 -p ${BIND_DATA_DIR}
 
   # populate default bind configuration if it does not exist
   if [ ! -d ${BIND_DATA_DIR}/etc ]; then
@@ -17,11 +17,11 @@ create_bind_data_dir() {
   fi
   rm -rf /etc/bind
   ln -sf ${BIND_DATA_DIR}/etc /etc/bind
-  chmod -R u+rwX,go+rX,go-w ${BIND_DATA_DIR}
-  chown -R ${BIND_USER}:${BIND_USER} ${BIND_DATA_DIR}
+#  chmod -R u+rwX,go+rX,go-w ${BIND_DATA_DIR}
+#  chown -R ${BIND_USER}:${BIND_USER} ${BIND_DATA_DIR}
 
   if [ ! -d ${BIND_DATA_DIR}/lib ]; then
-    mkdir -p ${BIND_DATA_DIR}/lib
+    mkdir -m 0775 -p ${BIND_DATA_DIR}/lib
     chown ${BIND_USER}:${BIND_USER} ${BIND_DATA_DIR}/lib
   fi
   rm -rf /var/lib/bind
@@ -29,9 +29,9 @@ create_bind_data_dir() {
 }
 
 create_webmin_data_dir() {
-  mkdir -p ${WEBMIN_DATA_DIR}
-  chmod -R u+rwX,go+rX,go-w ${WEBMIN_DATA_DIR}
-  chown -R root:root ${WEBMIN_DATA_DIR}
+  mkdir -m 0775 -p ${WEBMIN_DATA_DIR}
+#  chmod -R u+rwX,go+rX,go-w ${WEBMIN_DATA_DIR}
+#  chown -R root:root ${WEBMIN_DATA_DIR}
 
   # populate the default webmin configuration if it does not exist
   if [ ! -d ${WEBMIN_DATA_DIR}/etc ]; then
